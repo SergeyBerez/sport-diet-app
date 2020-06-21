3538994760da12ba976d7aabdf1d3e9a44a8deb6
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-06-21 13:12:42 +0300

    add validation

--- js/main.js
+++ js/main.js
@@ -9,10 +9,11 @@ const inputHeight = document.querySelector('.input-data-height');
 const inputWeight = document.querySelector('.input-data-weight');
 
 // ===== 3 inpunts from page anthropometry-predRegist
+const inputsAntrop = document.querySelectorAll('.input-param');
 const inptPersonBirthday = document.querySelector('[ name="person-birthday"]');
 const inpPersonHeight = document.querySelector('[name="person-height"]');
 const inpPersonWeight = document.querySelector('[name="person-weight"]');
-// check box
+// ===============check box
 const inpMan = document.querySelector('#inp-man');
 const inpFemale = document.querySelector('#inp-female');
 const buttonAntrop = document.querySelector('.button-antrop a');
@@ -21,28 +22,43 @@ const selectHeight = document.querySelector('#input-data-height');
 const selectWeight = document.querySelector('#input-data-weight');
 const selecWeightgr = document.querySelector('#input-data-weight-gr');
 //  handlers inpunts from page anthropometry
-//render select
 
 if (inpMan !== null) {
-  inptPersonBirthday.addEventListener('click', function (e) {
-    let tag = e.target.dataset.birthday;
-    document.querySelector(`.${tag}`).parentElement.classList.toggle('hide');
-  });
-  inpPersonHeight.addEventListener('click', function (e) {
-    let tag = e.target.dataset.height;
+  inputsAntrop.forEach(elem => {
+    elem.addEventListener('click', function (e) {
+      let strParam = e.target.getAttribute('name').split('-').slice(1).join('');
 
-    document.querySelector(`.${tag}`).parentElement.classList.toggle('hide');
+      document
+        .querySelector(`.modal-data-${strParam}`)
+        .parentNode.classList.toggle('show-modal');
+    });
   });
-  inpPersonWeight.addEventListener('click', function (e) {
-    let tag = e.target.dataset.weight;
-    document.querySelector(`.${tag}`).parentElement.classList.toggle('hide');
-  });
+  //===============another approach
+  // inptPersonBirthday.addEventListener('click', function (e) {
+  //   let tag = e.target.dataset.birthday;
+  //   document
+  //     .querySelector(`.${tag}`)
+  //     .parentElement.classList.toggle('show-modal');
+  // });
+  // inpPersonHeight.addEventListener('click', function (e) {
+  //   let tag = e.target.dataset.height;
+
+  //   document
+  //     .querySelector(`.${tag}`)
+  //     .parentElement.classList.toggle('show-modal');
+  // });
+  // inpPersonWeight.addEventListener('click', function (e) {
+  //   let tag = e.target.dataset.weight;
+  //   document
+  //     .querySelector(`.${tag}`)
+  //     .parentElement.classList.toggle('show-modal');
+  // });
 
   // =======handlers modal inpunt
   //close  modal
   btnChancelAll.forEach(element => {
     element.addEventListener('click', function (e) {
-      e.target.closest('.overlay').classList.toggle('hide');
+      e.target.closest('.overlay').classList.toggle('show-modal');
     });
   });
   // set value birthday
@@ -55,8 +71,10 @@ if (inpMan !== null) {
       },
     );
     inptPersonBirthday.value = str;
+    inptPersonBirthday.classList.remove('error');
+    inptPersonBirthday.previousElementSibling.textContent = '';
     if (inptPersonBirthday.value != '') {
-      e.target.parentElement.closest('.overlay').classList.toggle('hide');
+      e.target.parentElement.closest('.overlay').classList.toggle('show-modal');
     }
   });
 
@@ -64,9 +82,10 @@ if (inpMan !== null) {
   buttonHight.addEventListener('click', function (e) {
     inpPersonHeight.value = '';
     inpPersonHeight.value = `${selectHeight.value} cm`;
-    selectHeight.value;
+    inpPersonHeight.classList.remove('error');
+    inpPersonHeight.previousElementSibling.textContent = '';
     if (selectHeight.value != '') {
-      e.target.parentElement.closest('.overlay').classList.toggle('hide');
+      e.target.parentElement.closest('.overlay').classList.toggle('show-modal');
     }
   });
 
@@ -75,8 +94,10 @@ if (inpMan !== null) {
     inpPersonWeight.value = '';
     //inpPersonWeight.value = inputWeight.value;
     inpPersonWeight.value = `${selectWeight.value}.${selecWeightgr.value} kg`;
+    inpPersonWeight.classList.remove('error');
+    inpPersonWeight.previousElementSibling.textContent = '';
     if (selectHeight.value != '') {
-      e.target.parentElement.closest('.overlay').classList.toggle('hide');
+      e.target.parentElement.closest('.overlay').classList.toggle('show-modal');
     }
   });
 
@@ -94,32 +115,56 @@ if (inpMan !== null) {
     op.textContent = i;
     selectWeight.insertAdjacentElement('afterbegin', op);
   }
-  for (let i = 1; i < 9; i++) {
+  let j = 100;
+  for (let i = 0; i < 9; i++) {
     let op = document.createElement('option');
-    let j = i + '00';
-    op.value = +j;
-    op.textContent = +j;
+
+    op.value = j;
+    op.textContent = j;
+    j += 100;
     selecWeightgr.insertAdjacentElement('afterbegin', op);
   }
 
   // validation   ===========checked box
-  inpMan.addEventListener('click', function (e) {
-    console.log(inpMan.checked);
-  });
-  inpFemale.addEventListener('click', function (e) {
-    console.log(inpFemale.checked);
-  });
+  // inpMan.addEventListener('click', function (e) {
+  //   console.log(inpMan.checked);
+  // });
+  // inpFemale.addEventListener('click', function (e) {
+  //   console.log(inpFemale.checked);
+  // });
+
+  // ======calculate  user kkcal day  // ang go next page
 
   buttonAntrop.addEventListener('click', function (e) {
     if (!inpMan.checked && !inpFemale.checked) {
-      // e.preventDefault();
+      e.preventDefault();
+      inpMan.addEventListener('click', function (e) {
+        if (!inpFemale.checked) {
+          inpFemale.nextElementSibling.classList.remove('error-icon');
+        }
+      });
+      inpFemale.addEventListener('click', function (e) {
+        if (!inpMan.checked) {
+          inpMan.nextElementSibling.classList.remove('error-icon');
+        }
+      });
+
+      if (!inpMan.checked) {
+        inpMan.nextElementSibling.classList.add('error-icon');
+      }
+      if (!inpFemale.checked) {
+        inpFemale.nextElementSibling.classList.add('error-icon');
+      }
     }
-    if (
-      inpPersonHeight.value == '' ||
-      inpPersonWeight.value == '' ||
-      inptPersonBirthday.value == ''
-    ) {
-      // e.preventDefault();
-    }
+    inputsAntrop.forEach(elem => {
+      if (elem.value == '') {
+        e.preventDefault();
+        elem.classList.add('error');
+        elem.previousElementSibling.textContent = 'заполните данные';
+      } else {
+        elem.classList.remove('error');
+        elem.previousElementSibling.textContent = '';
+      }
+    });
   });
 }
